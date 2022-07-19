9f46f78027c6140f2dfd024dec99f37a606e38db
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-07-19 17:38:48 +0000

    Create listener for auth in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentUser } from "./features/user/userSlice";
+import HomePage from "./HomePage";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "./utils/firebaseUtils";
+
 function App() {
-  return <div className="App">App</div>;
+  const { currentUser } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+        dispatch(setCurrentUser(user?.uid));
+      } else {
+        dispatch(setCurrentUser(null));
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <div className="App">
+      {currentUser}
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
